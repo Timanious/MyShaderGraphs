94ca3165eae7dee19e6b98770e37b3a82bbd763c
Timanious <timcoster@gmail.com>
2019-09-19 12:45:59 +0200

    Upgraded to Unity 2019.2.5f1
    Upgraded to Unity 2019.2.5f1 and removed unnecessary assets.

---
+++ MyShaderGraphs_Project/Assets/_ShaderGraphs/SkyboxShaders/ProceduralSky/5-ProceduralSky (Complete)/ProceduralSky (Scripts)/DirectionalIntensity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+[ExecuteInEditMode]
+public class DirectionalIntensity : MonoBehaviour
+{
+    public Light directionalLight;
+    public AnimationCurve brightnessCurve = new AnimationCurve(new Keyframe(0,0.01f),new Keyframe(0.45f,0.01f),new Keyframe(1,1));
+
+    private void Awake() 
+    {
+        if(directionalLight.type != LightType.Directional)
+        {
+            Debug.Log("Light is not of type Diretional, setting it to directional.. ");
+            directionalLight.type = LightType.Directional;
+        }
+    }
+
+    void Update()
+    {
+        float angle = Vector3.Dot(Vector3.down,directionalLight.transform.forward.normalized); // range -1 <> 1
+        angle = angle * .5f + .5f; // remap range to 0 <> 1
+        directionalLight.intensity = brightnessCurve.Evaluate(angle);
+    }
+}
